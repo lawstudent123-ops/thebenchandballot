0fe8e839f3c94430b90208d366c4a52c2218baae
lawstudent123-ops <yolo2029292929@gmail.com>
2026-08-02 17:47:55 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,62 +1,99 @@
-import { getAuth, onAuthStateChanged } from 
+import { 
+getAuth, 
+onAuthStateChanged,
+signOut
+} from 
 "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
 
 
 const auth = getAuth();
 
 
+const nav = document.querySelector("nav");
 
-const loginLink = document.getElementById("loginLink");
 
-const profileLink = document.getElementById("profileLink");
-
+if(nav){
 
 
 onAuthStateChanged(auth,(user)=>{
 
 
+// Remove old auth buttons
+
+const oldAuth =
+document.getElementById("authButton");
+
+
+const oldProfile =
+document.getElementById("profileButton");
+
+
+if(oldAuth){
+oldAuth.remove();
+}
+
+
+if(oldProfile){
+oldProfile.remove();
+}
+
+
+
+
 if(user){
 
 
-if(loginLink){
+nav.innerHTML += `
 
-loginLink.innerHTML="Sign Out";
+<a id="profileButton" href="profile.html">
+Profile
+</a>
 
-loginLink.href="#";
+
+<a id="authButton" href="#">
+Sign Out
+</a>
+
+`;
+
+
+
+document
+.getElementById("authButton")
+.onclick = async()=>{
+
+
+await signOut(auth);
+
+
+window.location.reload();
+
+
+};
+
 
 }
 
 
-if(profileLink){
-
-profileLink.style.display="inline";
-
-}
-
-
-
-}
 
 else{
 
 
-if(loginLink){
+nav.innerHTML += `
 
-loginLink.innerHTML="Sign In";
+<a id="authButton" href="login.html">
+Sign In
+</a>
 
-loginLink.href="login.html";
-
-}
+`;
 
 
-if(profileLink){
-
-profileLink.style.display="none";
 
 }
 
-
-}
 
 
 });
+
+
+}
